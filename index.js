5a22e8548d15b11d489ede12c6605fa99edccdb6
Jared Grippe <jared@deadlyicon.com>
2014-04-26 00:24:51 -0700

    WIP

--- index.js
+++ index.js
@@ -41,43 +41,15 @@ $(function() {
   imagePost = new ImagePost;
 
   $('#input').keyup(function(event) {
+    update($(this).val());
+  });
 
-    imagePost.attr('text', $(this).val());
+  function update(text) {
+    imagePost.attr('text', text);
     var canvas = imagePost.toCanvas();
     $("#canvas-container").html(canvas);
     var image = imagePost.toImage();
     $("#image-container").html(image);
-
-
-
-  });
+  };
 
 });
-
-
-
-function update(text){
-  var fontSize = 20;
-  var canvas = $('<canvas>')[0];
-  var context = canvas.getContext("2d");
-  var top = 50;
-
-  context.font = fontSize+"px Georgia";
-  text.split("\n").forEach(function(line) {
-    context.fillText(line, 10, top);
-    top = top + fontSize;
-  });
-
-
-
-  // var textSize = context.measureText(text);
-  // console.log('height', textSize.height);
-  // console.log('width', textSize.width);
-
-  $("#canvas-container").html(canvas);
-
-  var image = new Image;
-  image.src = canvas.toDataURL("image/png");
-  $("#image-container").html(image);
-}
-
