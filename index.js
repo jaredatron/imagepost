d84e48a4089337832b953fa77d91163837f7b955
Jared Grippe <jared@deadlyicon.com>
2014-04-26 00:22:55 -0700

    WIP

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
+ImagePost = function() {
+  this.attr('font-size', 20);
+  this.attr('font-family', 'Georgia');
+};
+
+ImagePost.prototype.attr = function(attribute, value) {
+  var property = '_'+attribute;
+  if (arguments.length > 1){
+    this[property] = value;
+    return this;
+  }
+  return this[property];
+};
+
+ImagePost.prototype.toCanvas = function() {
+  var fontSize = this.attr('font-size');
+  var fontFamily = this.attr('font-family');
+  var top = 50;
+  var text = this.attr('text');
+
+  var canvas = $('<canvas>')[0];
+  var context = canvas.getContext("2d");
+
+  context.font = fontSize+"px "+fontFamily;
+  text.split("\n").forEach(function(line) {
+    context.fillText(line, 10, top);
+    top = top + fontSize;
+  });
+  return canvas;
+};
+
+ImagePost.prototype.toImage = function() {
+  var image = new Image;
+  image.src = this.toCanvas().toDataURL("image/png");
+  return image;
+};
+
+
 $(function() {
+
+  imagePost = new ImagePost;
 
   $('#input').keyup(function(event) {
 
-    update($(this).val());
+    imagePost.attr('text', $(this).val());
+    var canvas = imagePost.toCanvas();
+    $("#canvas-container").html(canvas);
+    var image = imagePost.toImage();
+    $("#image-container").html(image);
+
+
 
   });
 
