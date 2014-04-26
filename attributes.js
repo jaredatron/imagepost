70626310935ad4db58654f8a17edd362651dd55c
Jared Grippe <jared@deadlyicon.com>
2014-04-26 01:09:34 -0700

    WIP

--- attributes.js
+++ attributes.js
@@ -1,22 +1,35 @@
 function Attributes() {
+  var instance = this instanceof Attributes ? this :
+    Object.create(Attributes.prototype)
 
-  var attributes = function(key, value) {
-    key = '_'+key;
-    if (arguments.length < 2) return attributes.values[key];
-    attributes.values[key] = value;
+  instance.values = {};
+
+  function attributes(key, value) {
+    if (arguments.length < 2) return this.get(key);
+    instance.set(key, value);
     return this;
   }
 
-  attributes.values = {};
+  attributes.values = instance.values;
+  attributes.delete = instance.delete.bind(instance);
+  attributes.get    = instance.get.bind(instance);
+  attributes.set    = instance.set.bind(instance);
 
-  attributes.delete = Attributes.prototype.delete;
+  return attributes
+};
 
-  return attributes;
+Attributes.prototype.get = function(key) {
+  return this.values[key];
+};
+
+Attributes.prototype.set = function(key, value) {
+  this.values[key] = value;
+  return this;
 };
 
 Attributes.prototype.delete = function(key) {
-  var value = this(key);
-  this.values.delete(key);
+  var value = this.values[key];
+  delete this.values[key];
   return value;
 };
 
